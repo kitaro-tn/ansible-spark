7bb183005e1e061930a4239438f761282d512694
tatsunori-nishikori <t.nishikori@cchan.tv>
2017-06-22 12:34:29 +0900

    add mysql jdbc connector

--- roles/spark/files/spark-env.sh
+++ roles/spark/files/spark-env.sh
@@ -3,3 +3,5 @@ export SPARK_HOME=/usr/local/src/spark-2.1.1-bin-hadoop2.7
 export SPARK_LOCAL_IP="127.0.0.1"
 
 PATH=$SPARK_HOME/bin:$SPARK_HOME/sbin:/usr/local/src/scala-2.11.11/bin:$PATH
+
+export CLASSPATH=/usr/local/lib/mysql-connector-java-5.1.42-bin.jar:$CLASSPATH
